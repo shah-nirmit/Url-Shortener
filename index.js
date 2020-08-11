612bb7b80c2abd8b2a9a38b7ddae38fc02fe95ee
Nirmit Shah <nirmitshah191@gmail.com>
2020-08-11 10:17:40 +0530

    initial commit

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("./public"));
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "naughty.sh - Short Urls for you ",
-  });
-});
+// app.get("/", (req, res) => {
+//   res.json({
+//     message: "naughty.sh - Short Urls for you ",
+//   });
+// });
 
-app.post("/url/:id", (req, res) => {
-  //todo:get a short url by id
-});
+// app.post("/url/:id", (req, res) => {
+//   //todo:get a short url by id
+// });
 
-app.get("/:id", (req, res) => {
-  //todo:redirect to the url
-});
+// app.get("/:id", (req, res) => {
+//   //todo:redirect to the url
+// });
 
-app.post("/url", (req, res) => {
-  //todo:create a short url
-});
+// app.post("/url", (req, res) => {
+//   //todo:create a short url
+// });
 
 const port = process.env.port || 1337;
 app.listen(port, () => {
