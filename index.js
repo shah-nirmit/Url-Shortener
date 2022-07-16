7f36c0a5db4d1d4f24950e1cccc8dd9e158c3797
naughtyb0y <naughtyboy2k18@gmail.com>
2022-07-16 12:18:57 +0530

    Enhanced healthcheck
    Signed-off-by: naughtyb0y <naughtyboy2k18@gmail.com>

--- index.js
+++ index.js
@@ -105,6 +105,7 @@ app.post(
 
 app.get("/healthcheck", async (req, res, next) => {
   res.json("A healthcheck")
+  res.status(200)
 })
 app.use((req, res, next) => {
   res.status(404).sendFile(notFoundPath);
